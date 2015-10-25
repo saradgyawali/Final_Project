f960e28476edac303880f7cb89ba00d13961a5a0
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-10-25 13:53:02 -0700

    generated workouts and days controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+days = Day.create([{ name: 'Sunday'} ,{ name: 'Monday'}, { name: 'Tuesday'}, { name: 'Wednesday'},
+{ name: 'Wednesday'}, { name: 'Thursday'}, { name: 'Friday'}, { name: 'Saturday'} ])
